d296253c2609772fb600e5042e11a4ea3d2e0727
ilya <zapaska96@gmail.com>
2022-10-20 21:17:29 +0500

    v1.05 settings

--- src/components/PomodoroPanel.tsx
+++ src/components/PomodoroPanel.tsx
@@ -8,9 +8,11 @@ import { SettingsModal } from './SettingsModal';
 
 export const PomodoroPanel = () => {
     const [startPressed, setStartPressed] = useState(false);
+    const [settingsWindowOpened, setSettingsWindowOpened] = useState(false);
+    const [modesListState, setModesListState] = useState(initialModesList);
 
     const { currentMode, nextMode, setNeededMode } =
-        usePomodoroMode(initialModesList);
+        usePomodoroMode(modesListState);
 
     const timeOut = () => {
         nextMode();
@@ -39,6 +41,43 @@ export const PomodoroPanel = () => {
         setStartPressed(false);
     };
 
+    const changeTimeHandler = (p: string, s: string, l: string) => {
+        const newModesList = [
+            {
+                id: 1,
+                type: 'work',
+                // seconds: 1500,
+                title: 'Время работать!',
+                seconds: +p * 60,
+                panel_bg: 'bg-green-600',
+                app_bg: 'bg-green-500',
+                app_color: 'text-green-500',
+            },
+
+            {
+                id: 2,
+                type: 'short_break',
+                title: 'Время отдыхать!',
+                seconds: +s * 60,
+                panel_bg: 'bg-blue-600',
+                app_bg: 'bg-blue-500',
+                app_color: 'text-blue-500',
+            },
+
+            {
+                id: 3,
+                type: 'long_break',
+                title: 'Время конкретно отдыхать!',
+                seconds: +l * 60,
+                panel_bg: 'bg-indigo-600',
+                app_bg: 'bg-indigo-500',
+                app_color: 'text-indigo-500',
+            },
+        ];
+        setSettingsWindowOpened(false);
+        setModesListState(newModesList);
+    };
+
     return (
         <div
             className={`min-h-screen flex justify-center items-center relative ${currentMode.panel_bg}`}
@@ -46,6 +85,16 @@ export const PomodoroPanel = () => {
             <div
                 className={`flex flex-col justify-center items-center gap-8 py-4 px-16 text-white rounded ${currentMode.app_bg}`}
             >
+                <SettingsModal
+                    visible={settingsWindowOpened}
+                    changeTimeHandler={changeTimeHandler}
+                />
+                <button
+                    className="absolute top-10 right-10"
+                    onClick={() => setSettingsWindowOpened((prev) => !prev)}
+                >
+                    Settings
+                </button>
                 <div className="flex justify-around items-center gap-3">
                     <ModeButton
                         isCurrent={currentMode.id === 1}
---
+++ src/components/SettingsModal.tsx
@@ -0,0 +1,77 @@
+import React, { useRef } from 'react';
+import { SettingsInput } from './SettingsInput';
+
+export type OptionsType = {
+    id: string;
+    text: string;
+    value: number;
+    minValue?: number;
+    maxValue?: number;
+};
+
+export type SettingsModalProps = {
+    visible: boolean;
+    changeTimeHandler: (a: string, b: string, c: string) => void;
+};
+
+export const SettingsModal: React.FC<SettingsModalProps> = ({
+    visible,
+    changeTimeHandler,
+}) => {
+    const pomodoroRef = useRef<HTMLInputElement>(null);
+    const shortBreakRef = useRef<HTMLInputElement>(null);
+    const longBreakRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = () => {
+        if (
+            !pomodoroRef.current?.value ||
+            !shortBreakRef.current?.value ||
+            !longBreakRef.current?.value
+        )
+            return;
+        changeTimeHandler(
+            pomodoroRef.current.value,
+            shortBreakRef.current.value,
+            longBreakRef.current.value
+        );
+    };
+
+    if (!visible) return <></>;
+
+    return (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div className="bg-gray-50 rounded text-black max-w-[500px] px-4 py-4">
+                <div className="flex justify-between items-center">
+                    <h1>Настройки таймера</h1>
+                    <button>Close</button>
+                </div>
+                <hr />
+                <div className="flex justify-around items-center gap-4 mt-2">
+                    <div className="flex-1">
+                        <label htmlFor={'pomodoro'}>Помодоро</label>
+                        <SettingsInput
+                            id={'pomodoro'}
+                            value={20}
+                            ref={pomodoroRef}
+                        />
+                    </div>
+                    <div className="flex-1">
+                        <label htmlFor={'short_break'}>Короткий перерыв</label>
+                        <SettingsInput
+                            id={'short_break'}
+                            value={5}
+                            ref={shortBreakRef}
+                        />
+                    </div>
+                    <div className="flex-1">
+                        <label htmlFor={'long_break'}>Длинный перерыв</label>
+                        <SettingsInput id={''} value={15} ref={longBreakRef} />
+                    </div>
+                </div>
+                <div className="grid items-center mt-2">
+                    <button onClick={submitHandler}>Подтвердить</button>
+                </div>
+            </div>
+        </div>
+    );
+};
