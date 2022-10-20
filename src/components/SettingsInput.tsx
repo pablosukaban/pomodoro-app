769a3f1e6eaa5ed58b9f1ef3f25e9c239f5fecf3
ilya <zapaska96@gmail.com>
2022-10-20 21:16:20 +0500

    settings input file

---
+++ src/components/SettingsInput.tsx
@@ -0,0 +1,24 @@
+import React, { forwardRef } from 'react';
+
+export type SettingsInputProps = {
+    id: string;
+    value?: number;
+};
+
+export type Ref = HTMLInputElement;
+
+// eslint-disable-next-line react/display-name
+export const SettingsInput = forwardRef<Ref, SettingsInputProps>(
+    ({ id }, ref) => {
+        return (
+            <input
+                min={1}
+                id={id}
+                ref={ref}
+                type={'number'}
+                className="w-full px-2 py-2"
+                placeholder="Время в минутах"
+            />
+        );
+    }
+);
