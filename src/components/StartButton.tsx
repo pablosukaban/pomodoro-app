3ccd92b02dd83e59b6ce53832ebb4472c3fb4400
ilya <zapaska96@gmail.com>
2022-10-20 21:15:29 +0500

    rename

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 export type StartButtonProps = {
     text: string;
+    app_color: string;
     isPressed?: boolean;
     onClick: () => void;
 };
 
 export const StartButton: React.FC<StartButtonProps> = ({
     text,
+    app_color,
     onClick,
     isPressed,
 }) => {
     return (
         <button
-            className={`text-green-500 bg-white rounded w-3/4 py-4 transition-all active:translate-y-0.5 active:border-none ${
+            className={`${app_color} bg-white rounded w-3/4 py-4 transition-all active:translate-y-0.5 active:border-none ${
                 isPressed
                     ? 'translate-y-0.5 border-none '
                     : 'border-b-[6px] border-b-gray-300 '
